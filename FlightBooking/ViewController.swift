ffd7bac2c5047a2bf0aef2667580f55a6c865d60
Eddie <eahnivy8@gmail.com>
2019-09-15 19:43:35 +0900

    Initial Commit

---
+++ FlightBooking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightBooking
+//
+//  Created by Eddie Ahn on 15/09/2019.
+//  Copyright © 2019 Eddie Ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
