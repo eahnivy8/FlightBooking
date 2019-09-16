43c21792c7b756914ee6f537d6784dadf9afa8ae
Eddie <eahnivy8@gmail.com>
2019-09-16 11:45:53 +0900

    Flight Booking App Completed.

--- FlightBooking/ViewController.swift
+++ FlightBooking/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var buttonTag = 1
+    @IBOutlet var departureDateButton: UIButton!
+    @IBOutlet var returnDateLabel: UILabel!
+    @IBOutlet var returnDateButton: UIButton!
+    @IBOutlet var selectDatePicker: UIDatePicker!
+    
+    @IBAction func showDatePickerAction(_ sender: UIButton) {
+        if selectDatePicker.isHidden == false {
+            selectDatePicker.isHidden = true
+        } else {
+            selectDatePicker.isHidden = false
+        }
+        buttonTag = sender.tag
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        returnDateLabel.isHidden = true
+        returnDateButton.isHidden = true
+        selectDatePicker.isHidden = true
     }
-
-
+    
+    @IBAction func showReturnDateAction(_ sender: UISwitch) {
+        //        if sender.isOn {
+        //            returnDateButton.isHidden = false
+        //            returnDateLabel.isHidden = false
+        //        } else {
+        //            returnDateButton.isHidden = true
+        //            returnDateLabel.isHidden = true
+        //        }
+        returnDateButton.isHidden = !sender.isOn
+        returnDateLabel.isHidden = !sender.isOn
+    }
+    
+    @IBAction func selectedDateAction(_ sender: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YY-MM-d hh:mma"
+        let dateString = formatter.string(from: sender.date)
+        if buttonTag == 1 {
+            departureDateButton.setTitle(dateString, for:.normal)
+        } else {
+            returnDateButton.setTitle(dateString, for: .normal)
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        self.selectDatePicker.isHidden = true
+    }
 }
+
 
